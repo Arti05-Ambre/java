0db3520c679971325784bd0c853310beb5db2ca8
Arti05-Ambre <ambrearti62@gmail.com>
2026-05-25 08:01:33 +0530

    using data types

---
+++ src/basicofjava/Datatype.java
@@ -0,0 +1,12 @@
+package basicofjava;
+
+public class Datatype {
+    static void main(String[] args) {
+        int age=20;
+        float height=5.3f;
+        System.out.println("Age:"+age);
+    }
+
+    }
+
+
