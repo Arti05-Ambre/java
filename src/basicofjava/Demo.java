c05fc39b200a19787d1bf52f247160cadd6f4034
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-01 22:54:55 +0530

    variables in java int

--- src/basicofjava/Demo.java
+++ src/basicofjava/Demo.java
@@ -1,7 +1,7 @@
 package basicofjava;
 
-public class Demo {
-    public static void main(String[] args){
+public class  Demo {
+    public static void main(String[]  args){
         System.out.println("Hello World");
     }
 }
---
+++ src/basicofjava/Variables.java
@@ -0,0 +1,9 @@
+package basicofjava;
+
+public class Variables {
+     static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+
+    }
+}
