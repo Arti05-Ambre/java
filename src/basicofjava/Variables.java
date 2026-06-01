6f7721421e1bf20a300346ca94f2c7ee6d5c8094
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-01 23:06:43 +0530

    some changes in variables

--- src/basicofjava/Variables.java
+++ src/basicofjava/Variables.java
@@ -4,7 +4,9 @@ public class Variables {
      static void main(String[] args) {
         int a = 5;
         a=56;
+        int b = 23;
         System.out.println(a);
+         System.out.println(b);
 
     }
 }
