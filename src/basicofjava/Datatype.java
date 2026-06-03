fe75007ada65773bf477c3b9c99deb83030b1870
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-03 22:31:37 +0530

    All primitive data type in one frame

--- src/basicofjava/Datatype.java
+++ src/basicofjava/Datatype.java
@@ -3,8 +3,21 @@ package basicofjava;
 public class Datatype {
     static void main(String[] args) {
         int age=20;
-        float height=5.3f;
+        byte  year= 1;
+        short  a = 34;
+        long  number = 345678765L;
+        float temp = 34.56f;
+        double per = 99.99;
+        char grade = 'A';
+        boolean JavaIsFun = true;
         System.out.println("Age:"+age);
+        System.out.println(year);
+        System.out.println(a);
+        System.out.println(number);
+        System.out.println(temp);
+        System.out.println(per);
+        System.out.println(grade);
+        System.out.println(JavaIsFun);
     }
 
     }
