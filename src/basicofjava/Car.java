c0d0bfcfdb1b37839943bac42092c033035e7c39
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-03 23:00:08 +0530

    practice with class

--- src/basicofjava/Car.java
+++
@@ -1,7 +0,0 @@
-package basicofjava;
-
-public class Car {
-    String brand = "Toyato";
-    String model = "Toyato_camry";
-    int year = 2023;
-}
---
+++ src/basicofjava/Student.java
@@ -0,0 +1,16 @@
+package basicofjava;
+
+import java.sql.SQLOutput;
+
+public class Student {
+    public static void main(String[] args) {
+
+
+        String name = "Arti_Dipak_Ambre";
+        int age = 20;
+        double marks = 99.20;
+        System.out.println("My Name is:"+name);
+        System.out.println("My age is:"+age);
+        System.out.println("Obtained marks these year is:"+marks);
+    }
+}
