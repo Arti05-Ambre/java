7da3088ed79c2556e713c36fd1ea57fdfc4f1834
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-03 22:35:33 +0530

    addition with two diff data type

--- src/basicofjava/Datatype.java
+++ src/basicofjava/Datatype.java
@@ -2,22 +2,9 @@ package basicofjava;
 
 public class Datatype {
     static void main(String[] args) {
-        int age=20;
-        byte  year= 1;
-        short  a = 34;
-        long  number = 345678765L;
-        float temp = 34.56f;
-        double per = 99.99;
-        char grade = 'A';
-        boolean JavaIsFun = true;
-        System.out.println("Age:"+age);
-        System.out.println(year);
-        System.out.println(a);
-        System.out.println(number);
-        System.out.println(temp);
-        System.out.println(per);
-        System.out.println(grade);
-        System.out.println(JavaIsFun);
+       int a = 10;
+       double b = 5.5;
+        System.out.println(a+b);
     }
 
     }
