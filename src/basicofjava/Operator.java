3a4e078a123980ad32ed68caedce5b8a675ada61
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-08 16:06:16 +0530

    we can use operator in different from like above

--- src/basicofjava/Operator.java
+++ src/basicofjava/Operator.java
@@ -2,18 +2,19 @@ package basicofjava;
 
 public class Operator {
     static void main(String[] args) {
-       int n = 34;
-       int m = 56;
-       int r1 = n+m;
-       int r2 =  m-n;
-       int r3 = m*n;
-       int r4 = m/n;
-       int r5  = m%n;
-        System.out.println(r1);
-        System.out.println(r2);
-        System.out.println(r3);
-        System.out.println(r4);
-        System.out.println(r5);
+       int n = 2;
+       int m = 6;
+        n += m;
+        System.out.println(n);
+        n =n+m;
+        System.out.println(n);
+        n++;
+        System.out.println(n);
+        // n = n+m;
+        //n+=m;
+        //n++
+
+
 
     }
 }
