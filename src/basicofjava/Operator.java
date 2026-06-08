acf8613dd91d919c3d89b4d2642d05de816c75c5
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-08 15:30:02 +0530

    Arithmetic Operator

---
+++ src/basicofjava/Operator.java
@@ -0,0 +1,19 @@
+package basicofjava;
+
+public class Operator {
+    static void main(String[] args) {
+       int n = 34;
+       int m = 56;
+       int r1 = n+m;
+       int r2 =  m-n;
+       int r3 = m*n;
+       int r4 = m/n;
+       int r5  = m%n;
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+        System.out.println(r5);
+
+    }
+}
