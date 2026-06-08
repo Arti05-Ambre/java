f7e88e1a747e0698dc5e8c73c03903b91136cbc2
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-08 21:51:47 +0530

    Ternaryoperator

---
+++ src/basicofjava/Ternaryop.java
@@ -0,0 +1,10 @@
+package basicofjava;
+
+public class Ternaryop {
+    static void main(String[] args) {
+        int i = 5;
+        int j = 0;
+        j = i<6?1:2;
+        System.out.println(j);
+    }
+}
