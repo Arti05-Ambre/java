cfcd487c88974ce0fcf034f964b7d87d99fd837b
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-08 21:53:10 +0530

    Ternaryoperat

--- src/basicofjava/Ternaryop.java
+++ src/basicofjava/Ternaryop.java
@@ -2,7 +2,7 @@ package basicofjava;
 
 public class Ternaryop {
     static void main(String[] args) {
-        int i = 5;
+        int i = 7;
         int j = 0;
         j = i<6?1:2;
         System.out.println(j);
