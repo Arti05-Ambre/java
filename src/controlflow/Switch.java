ba81a166dab4ea997834aae8ece72ab77d58c58b
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-15 22:33:56 +0530

    new file

---
+++ src/controlflow/Switch.java
@@ -0,0 +1,14 @@
+package controlflow;
+
+public class Switch {
+    static void main() {
+        int a = 2;
+        System.out.println("one");
+        System.out.println("Two");
+        System.out.println("Three");
+        System.out.println("Four");
+        System.out.println("Five");
+        System.out.println("Six");
+        System.out.println("Seven");
+    }
+}
