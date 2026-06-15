6809db4b200142110bd37631d9c0b7a91f783c2c
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-15 22:17:42 +0530

    ifelseifelse

---
+++ src/controlflow/IF_lse_if_else.java
@@ -0,0 +1,15 @@
+package controlflow;
+
+public class IF_lse_if_else {
+    static void main(String[] args) {
+        int  n = 0;
+        if(n==0){
+            System.out.println("Nothing");
+        }else if(n%2==0){
+            System.out.println("N is positive");
+        }else{
+            System.out.println("N is negative");
+
+        }
+    }
+}
