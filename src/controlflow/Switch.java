840641736e9732b5e01334184399aa0cfd2dd9b8
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-15 22:38:46 +0530

    switch and case

--- src/controlflow/Switch.java
+++ src/controlflow/Switch.java
@@ -3,12 +3,21 @@ package controlflow;
 public class Switch {
     static void main() {
         int a = 2;
-        System.out.println("one");
-        System.out.println("Two");
-        System.out.println("Three");
-        System.out.println("Four");
-        System.out.println("Five");
-        System.out.println("Six");
-        System.out.println("Seven");
+        switch(a) {
+            case 1:
+                System.out.println("one");
+            case 2:
+                System.out.println("Two");
+            case 3:
+               System.out.println("Three");
+            case 4:
+                System.out.println("Four");
+            case 5:
+                System.out.println("Five");
+            case 6:
+               System.out.println("Six");
+            case 7:
+               System.out.println("Seven");
+        }
     }
 }
