446bca651ba4bf2598232cb02a03630f00f6f275
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-15 22:06:59 +0530

    if else control statement

---
+++ src/controlflow/IF_else.java
@@ -0,0 +1,15 @@
+package controlflow;
+
+public class IF_else {
+    static void main(String[] args) {
+
+
+        int n = 7;
+        if (n % 2 == 0) {
+            System.out.println("n is ven number");
+        } else {
+            System.out.println("n is odd");
+        }
+
+    }
+}
