46175206a7aee251ff3bb92877ff8801262bd1ba
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-16 17:19:29 +0530

    add,sub,product,quotient in one

---
+++ src/basicofjava/Calculator.java
@@ -0,0 +1,16 @@
+package basicofjava;
+
+public class Calculator {
+    static void main(String[] args) {
+
+
+        int a = 50;
+        int b = 5;
+        System.out.println("Addition of two number is:"+(a+b));
+        System.out.println("Substraction of two number is:"+(a-b));
+        System.out.println("product of two number is:"+(a*b));
+        System.out.println("quotient of two number is:"+(a/b));
+
+
+    }
+}
