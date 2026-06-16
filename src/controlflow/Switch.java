f41475d43e31c4b1dbf86410f03dd3ca70157ea8
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-16 14:58:28 +0530

    break statement

--- src/controlflow/Switch.java
+++ src/controlflow/Switch.java
@@ -2,22 +2,30 @@ package controlflow;
 
 public class Switch {
     static void main() {
-        int a = 2;
+        int a = 8;
         switch(a) {
             case 1:
                 System.out.println("one");
+                break;
             case 2:
                 System.out.println("Two");
+                break;
             case 3:
                System.out.println("Three");
+               break;
             case 4:
                 System.out.println("Four");
+                break;
             case 5:
                 System.out.println("Five");
+                break;
             case 6:
                System.out.println("Six");
+               break;
             case 7:
                System.out.println("Seven");
+               break;
+
         }
     }
 }
