1c9a6ff5f6d0fb1bdf31c920a061d340242e34ce
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-17 17:18:36 +0530

    variables practice

---
+++ src/basicofjava/Info.java
@@ -0,0 +1,13 @@
+package basicofjava;
+
+public class Info {
+    static void main(String[] args) {
+        String name = "Ambre Arti Dipak";
+        int age = 20;
+        double height = 5.3;
+        System.out.println("My name is:" + name);
+        System.out.println("My age is :" + age);
+        System.out.println("My height is:" + height);
+
+    }
+}
